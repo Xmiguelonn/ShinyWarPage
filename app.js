26706289a50913247a7a2fb4f976dc8442d067f4
Xmiguelonn <145677972+Xmiguelonn@users.noreply.github.com>
2026-07-20 13:13:09 +0200

    agregar contador

--- app.js
+++ app.js
@@ -37,3 +37,35 @@ onValue(scoresRef, (snapshot) => {
     });
     document.body.classList.add("connected");
 });
+
+
+
+
+const fechaInicio = new Date("2026-08-01T00:00:00+02:00").getTime();
+
+const countdownInterval = setInterval(() => {
+
+    const ahora = new Date().getTime();
+    const distancia = fechaInicio - ahora;
+
+    const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = String(dias).padStart(2, "0");
+    document.getElementById("hours").textContent = String(horas).padStart(2, "0");
+    document.getElementById("minutes").textContent = String(minutos).padStart(2, "0");
+    document.getElementById("seconds").textContent = String(segundos).padStart(2, "0");
+
+    if (distancia < 0) {
+
+        clearInterval(countdownInterval);
+        const container = document.getElementById("countdown-container");
+
+        container.innerHTML = `<h2 class="countdown-title-active">!LA SHINY WAR HA COMENZADO¡ 🔥</h2>`;
+    }
+
+
+
+}, 1000);
